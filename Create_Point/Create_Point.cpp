3145f7799dee695ae9a529c07ad8eb3f5e7d22a5
Andrhan04 <andrham2004@yandex.ru>
2024-10-16 12:56:29 +0800

    Update Create_Point.cpp

--- Create_Point/Create_Point.cpp
+++ Create_Point/Create_Point.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <iomanip>
 #include <vector>
 #include <algorithm>
@@ -7,22 +7,21 @@
 #include <fstream>
 #include <math.h>
 #include <random>
+#include <string>
 
 using namespace std;
-#define all(v) (v).begin(),(v).end()
 
 class Point {
 private:
     double eps = 1e-4;
     mt19937 random_generator_;
-    /*
+
     pair <double, double> Dif() {
         double Angle = Direction * acos(-1) / 180;
         double x = (cos(Angle) * Speed);
         double y = (sin(Angle) * Speed);
         return { x,y };
     }
-    */
 
     int returnRandom(int min, int max) {
         if (max < min) swap(max, min);
@@ -30,86 +29,290 @@ private:
         return range(random_generator_);
     }
 
-    void CreateCoord() {
-        //uniform_int_distribution die{ -10, 10 };
-        X = returnRandom(0, 1000);
-        X = -X;
-        Y = returnRandom(0, 1000);
-    }
-    /*
     void CreateSpeed() {
-        //uniform_int_distribution die{ 0, 100 };
-        Speed = returnRandom(0,100) / 100.0;
+        Speed = returnRandom(10, 100) / 100.0;
     }
 
     void CreateDirection() {
-        //uniform_int_distribution die{ 0, 359 };
-        Direction = returnRandom(0,359);
+        Direction = returnRandom(0, 359);
     }
-    */
+    struct Punch {
+        double X, Y;
+        bool was = false;
+    };
+
+    int InWorkZone(double x, double y) {
+        if (x < eps) {
+            return 1;
+        }
+        if (x > 50000.0) {
+            return 2;
+        }
+        if (y < 0.0) {
+            return 3;
+        }
+        if (y > 1000.0) {
+            return 4;
+        }
+        return 0;
+    }
+
+    int InBuferZone(double x, double y) {
+        if (x > 0.0) {
+            Work = true;
+            return 1;
+        }
+        if (x < -1000.0) {
+            return 2;
+        }
+        if (y < 0.0) {
+            return 3;
+        }
+        if (y > 1000.0) {
+            return 4;
+        }
+        return 0;
+    }
+
 
 public:
-    double X, Y; // 0 - 1000 координата Х, координата Y,
-    //double Speed; // 0.1 - 1 скорость,
-    //int Direction; // 0 - 359 направление.
+    double X, Y; // координата Х, координата Y,
+    double Speed; // 0.1 - 1 скорость,
+    int Direction; // 0 - 359 направление.
+    vector <Punch> v;
+    bool Work = false;
+    bool Alive = true;
 
     bool Same(Point* other) {
         return ((abs(other->X - X) < eps) && (abs(other->Y - Y) < eps));
     }
 
-    void Create() {
-        CreateCoord();
-        //CreateSpeed();
-        //CreateDirection();
-    }
-    /*
-    void iteration() {
-        pair<double, double> p = Dif();
-        X += p.first;
-        Y += p.second;
-        CreateSpeed();
+    void step() {
         CreateDirection();
+        if (!Work && Direction > 90 && Direction < 270) {
+            if (Direction > 180) {
+                Direction += 90;
+            }
+            else {
+                Direction -= 90;
+            }
+        }
+        auto [dx, dy] = Dif();
+
+        if (Work) { // Стенки, расстояние сохраняется
+            double new_x = X + dx, new_y = Y + dy;
+            int flag = InWorkZone(new_x, new_y);
+            while (flag != 0) {
+                Punch p;
+                if (flag == 1) {
+                    p.X = 0;
+                    p.Y = Y - (((new_y - Y) * X) / (new_x - X));
+                    new_x = abs(new_x);
+                }
+                else {
+                    if (flag == 2) {
+                        p.X = 50000.0;
+                        p.Y = ((p.X - X) / (new_x - X)) * (new_y - Y) + Y;
+                        new_x = 50000.0 - (new_x - 50000.0);
+                    }
+                    else {
+                        if (flag == 3) {
+                            p.Y = 0;
+                            p.X = X - (Y * (new_x - X) / (new_y - Y));
+                            new_y = abs(new_y);
+                        }
+                        else {
+                            p.Y = 1000.0;
+                            p.X = X + ((p.Y - Y) * (new_x - X) / (new_y - Y));
+                            new_y = 1000.0 - (new_y - 1000.0);
+                        }
+                    }
+                }
+                v.push_back(p);
+                X = p.X; Y = p.Y;
+                flag = InWorkZone(new_x, new_y);
+            }
+            X = new_x; Y = new_y;
+        }
+        else {
+            double new_x = X + dx, new_y = Y + dy;
+            int flag = InBuferZone(new_x, new_y);
+            while (flag) {
+                Punch p;
+                if (flag == 1) {
+                    Work = true;
+                    flag = InWorkZone(new_x + 1, new_y);
+                    if (flag) {
+                        Punch p;
+                        if (flag == 1) {
+                            p.X = 0;
+                            p.Y = Y - (((new_y - Y) * X) / (new_x - X));
+                            new_x = abs(new_x);
+                        }
+                        else {
+                            if (flag == 2) {
+                                p.X = 50000.0;
+                                p.Y = ((p.X - X) / (new_x - X)) * (new_y - Y) + Y;
+                                new_x = 50000.0 - (new_x - 50000.0);
+                            }
+                            else {
+                                if (flag == 3) {
+                                    p.Y = 0;
+                                    p.X = X - (Y * (new_x - X) / (new_y - Y));
+                                    new_y = abs(new_y);
+                                }
+                                else {
+                                    p.Y = 1000.0;
+                                    p.X = X + ((p.Y - Y) * (new_x - X) / (new_y - Y));
+                                    new_y = 1000.0 - (new_y - 1000.0);
+                                }
+                            }
+                        }
+                        v.push_back(p);
+                        X = p.X; Y = p.Y;
+                    }
+                }
+                else {
+                    if (flag == 2) {
+                        new_x = -1000.0 - (new_x + 1000.0);
+                    }
+                    else {
+                        if (flag == 3) {
+                            new_y = abs(new_y);
+                        }
+                        else {
+                            new_y = 1000.0 - (new_y - 1000.0);
+                        }
+                    }
+                }
+                v.push_back(p);
+                X = p.X; Y = p.Y;
+                if (Work) {
+                    flag = InWorkZone(new_x, new_y);
+                }
+                else{
+                    flag = InBuferZone(new_x, new_y);
+                }
+            }
+            X = new_x; Y = new_y;
+        }
     }
-    */
-    Point() {
+
+
+    Point(double x, double y) {
+        X = x;
+        Y = y;
+        Direction = 0;
         random_device device;
         random_generator_.seed(device());
-        Create();
+        CreateSpeed();
     }
 };
 
+
+class Trap
+{
+private:
+    double eps = 1e-4;
+    mt19937 random_generator_;
+
+    int returnRandom(int min, int max) {
+        if (max < min) swap(max, min);
+        std::uniform_int_distribution<int> range(min, max);
+        return range(random_generator_);
+    }
+
+public:
+    double X, Y, Range;
+    bool Alive;
+    Trap(double x, double y) {
+        random_device device;
+        random_generator_.seed(device());
+        X = x;
+        Y = y;
+        Range = 0.5;
+        Alive = true;
+    }
+
+    bool Catch(Point p) {
+        if ((p.X - X) * (p.X - X) + (p.Y - Y) * (p.Y - Y) < Range * Range) {
+            p.Alive = false;
+            Alive = false;
+            cout << "No alive " << p.Alive << ' '  << X << ' ' << Y << endl;
+            return true;
+        }
+        return false;
+    }
+
+
+};
+
+
 int main() {
-    ofstream Start("C:\\Corsache\\Sowing\\startPoints.txt");
-    if (!Start.is_open()) return 0;
-    int n = 1000, r = 0;
-    vector<Point*> v;
-    Start << "JSON\nTest2DStochastic" << endl;
-    for (int i = 0; i < n; i++) {
-        Point* p = new Point();
-        bool unic = true;
-        bool repeat = true;
-        while (repeat) {
-            for (auto i : v) {
-                if (p->Same(i)) {
-                    unic = false;
-                    break;
+    string line;
+    ifstream in_Point("C:\\Users\\student\\Desktop\\01321\\c++\\Sowing\\startPoints.txt");
+    ifstream in_Traps("C:\\Users\\student\\Desktop\\01321\\c++\\Sowing\\startTraps.txt");
+    ofstream Dead("MyFile.txt");
+    vector<Point> myArr;
+    vector<Trap> myTrap;
+    if (in_Point.is_open() && in_Traps.is_open())
+    {
+        getline(in_Point, line);
+        getline(in_Point, line);
+        while (getline(in_Point, line))
+        {
+            string X = line.substr(0, 9);
+            string Y = line.substr(9);
+            Point p = Point(stod(X), stod(Y));
+            myArr.push_back(p);
+        }
+        cerr << 1 << endl;
+        int t = 1000, cp_t;
+        cp_t = t;
+        getline(in_Traps, line);
+        getline(in_Traps, line);
+        getline(in_Traps, line);
+        while (getline(in_Traps, line))
+        {
+            string X = line.substr(0, 10);
+            string Y = line.substr(10);
+            Trap tr =  Trap(stod(X), stod(Y));
+            myTrap.push_back(tr);
+        }
+        cerr << 2 << endl;
+        int cnt = 0;
+        while (t--) {
+            for (auto i : myArr) {
+                if (i.Alive) {
+                    i.step();
+                    if (i.Work) {
+                        bool flag = false;
+                        for (auto tr : myTrap) {
+                            if (tr.Alive == 1) {                                
+                                if (tr.Catch(i)) {
+                                    Dead << setprecision(3) << fixed << setw(8) << cp_t - t << setw(8) << tr.X << setw(8) << tr.Y;
+                                    Dead << setprecision(3) << fixed << setw(8) << i.X <<  setw(8) << i.Y << ' ' << tr.Alive << endl;
+                                    cnt++;
+                                    flag = true;                                    
+                                }
+                            }
+                            if (flag) {
+                                cout << tr.Alive << endl;
+                                break;
+                            }
+                        }
+                    }
                 }
             }
-            if (!unic) {
-                p->Create();
-                unic = true;
-                r++;
-            }
-            else {
-                repeat = false;
-            }
+            //cout << t << endl;
         }
-        v.push_back(p);
-
-        Start << setprecision(3) << fixed << setw(8) << p->X << setw(8) << p->Y << setw(8) << endl;
-        cout << setprecision(3) << fixed << setw(8) << p->X << setw(8) << p->Y << setw(8) << endl;
-
+        cerr << 3 << endl;
+        Dead << cnt << endl;
     }
-    cout << r;
-    return 0;
+    else {
+        cout << "warning" << endl;
+    }
+    in_Point.close();
+    in_Traps.close();
+
 }
